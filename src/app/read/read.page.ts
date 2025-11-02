dd75ad51570c96af9fdd6580cda99b547ada4134
yandetoure <ndeyeyandemouhammad@gmail.com>
2025-11-02 14:31:02 +0000

    sidebar readbook

--- src/app/read/read.page.ts
+++ src/app/read/read.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ChapterService, ChapterDetail } from '../services/chapter.service';
+import { BookService, BookChapterSummary } from '../services/book.service';
 import { ReadingProgressService } from '../services/reading-progress.service';
 
 @Component({
@@ -13,10 +14,14 @@ export class ReadPage implements OnInit {
   chapter?: ChapterDetail;
   bookId!: string;
   chapterId!: string;
+  chapters: BookChapterSummary[] = [];
+  isMenuOpen = false;
 
   constructor(
     private readonly route: ActivatedRoute,
+    private readonly router: Router,
     private readonly chaptersApi: ChapterService,
+    private readonly booksApi: BookService,
     private readonly progress: ReadingProgressService,
   ) {}
 
@@ -28,6 +33,22 @@ export class ReadPage implements OnInit {
         this.chapter = c;
         this.progress.setProgress(this.bookId, this.chapterId, c.number);
       });
+      
+      // Charger la liste des chapitres du livre
+      this.booksApi.getBook(this.bookId).subscribe((book) => {
+        this.chapters = book.chapters || [];
+      });
+    }
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  selectChapter(ch: BookChapterSummary): void {
+    if (!ch.isPaid || ch.id === this.chapterId) {
+      this.router.navigate(['/tabs/read', this.bookId, ch.id]);
+      this.isMenuOpen = false;
     }
   }
 }
